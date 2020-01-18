ab0a2b38e45b5361b02741ac993e21b658fd3115
nebosite <ericjorg@thejcrew.net>
2020-01-18 12:17:28 -0800

    Version 0.0.3.1

--- src/globals.ts
+++ src/globals.ts
@@ -1,7 +1,7 @@
 
 export class GLOBALS
 {
-    static version = "0.0.3.0";
+    static version = "0.0.3.1";
     static INFO_Y_AREA = 100;
     static PLAYER_Y_AREA = 80;
     static SHIELD_Y_AREA = 60;
