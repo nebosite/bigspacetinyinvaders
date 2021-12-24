6c81b5a723097c3dbf732293d07d62737f76c5a3
Eric Jorgensen <ericjorg@thejcrew.net>
2021-12-23 20:35:00 -0800

    Version 0.0.7.0

--- src/globals.ts
+++ src/globals.ts
@@ -2,7 +2,7 @@
 export class GLOBALS
 {
     static debug = true;
-    static version = "0.0.6.0";
+    static version = "0.0.7.0";
     static INFO_Y_AREA = 100;
     static PLAYER_Y_AREA = 80;
     static SHIELD_Y_AREA = 50;
